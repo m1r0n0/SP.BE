dfb378e105feed8b5535952c7f6908ded9774c64
Oleksandr <olexandr.myroniuk@gmail.com>
2023-07-20 15:27:26 +0300

    Implemented user and provider deletion

--- SP.Provider/SP.Provider.BusinessLayer/Interfaces/IProviderService.cs
+++ SP.Provider/SP.Provider.BusinessLayer/Interfaces/IProviderService.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace SP.Provider.BusinessLayer.Interfaces
 {
@@ -12,5 +13,6 @@ namespace SP.Provider.BusinessLayer.Interfaces
         Task<DataAccessLayer.Models.Provider> CreateProvider(string userId, ProviderInfoDTO model);
         Task<ProviderInfoDTO> GetProvider(string userId);
         Task<ProviderInfoDTO> UpdateProvider(string userId, ProviderInfoDTO model);
+        Task DeleteProvider(string userId);
     }
 }
