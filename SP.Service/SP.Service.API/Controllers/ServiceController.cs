996cf7c8bb38fe2e64335fbff50b9b9fd9b41b5b
Oleksandr <olexandr.myroniuk@gmail.com>
2023-07-28 15:54:10 +0300

    28.07.23

---
+++ SP.Service/SP.Service.API/Controllers/ServiceController.cs
@@ -0,0 +1,103 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using SP.Customer.API.ViewModels;
+using SP.Customer.BusinessLayer.DTOs;
+using SP.Customer.BusinessLayer.Exceptions;
+using SP.Customer.BusinessLayer.Interfaces;
+using Swashbuckle.AspNetCore.Annotations;
+
+namespace SP.Customer.API.Controllers
+{
+    [Authorize]
+    [ApiController]
+    [Route("api/v1/customer/")]
+    public class ServiceController : ControllerBase
+    {
+        private readonly ICustomerService _customerService;
+
+        public ServiceController(ICustomerService customerService)
+        {
+            _customerService = customerService;
+        }
+
+        [HttpPost]
+        [Route("new/{userId}")]
+        [ProducesResponseType(typeof(CustomerDTO), 200)]
+        [ProducesResponseType(typeof(ModelErrorVM), 400)]
+        [SwaggerOperation(Summary = "Register the Customer")]
+        public async Task<IActionResult> RegisterCustomer(string userId, CustomerInfoDTO model)
+        {
+            try
+            {
+                var customer = await _customerService.CreateCustomer(userId, model);
+
+                return Ok(customer);
+            }
+            catch (ConflictException)
+            {
+                return Conflict(model);
+            }
+
+
+        }
+
+        [HttpGet]
+        [Route("{userId}")]
+        [ProducesResponseType(typeof(CustomerInfoDTO), 200)]
+        [SwaggerOperation(Summary = "Get the Customer")]
+        public async Task<IActionResult> GetCustomer(string userId)
+        {
+            try
+            {
+                var Customer = await _customerService.GetCustomer(userId);
+
+                return Ok(Customer);
+            }
+            catch (NotFoundException)
+            {
+                return NotFound();
+            }
+
+        }
+
+
+        [HttpPut]
+        [Route("{userId}")]
+        [SwaggerOperation(Summary = "Edit the Customer")]
+        [ProducesResponseType(typeof(CustomerInfoDTO), 200)]
+        [ProducesResponseType(typeof(ModelErrorVM), 400)]
+        [ProducesResponseType(404)]
+        public async Task<IActionResult> EditCustomer(string userId, CustomerInfoDTO model)
+        {
+            try
+            {
+                var customer = await _customerService.UpdateCustomer(userId, model);
+
+                return Ok(customer);
+            }
+            catch (NotFoundException)
+            {
+                return NotFound();
+            }
+        }
+
+        [HttpDelete]
+        [Route("{userId}")]
+        [SwaggerOperation(Summary = "Delete the Customer")]
+        [ProducesResponseType(200)]
+        public async Task<IActionResult> DeleteCustomer(string userId)
+        {
+            try
+            {
+                await _customerService.DeleteCustomer(userId);
+
+                return Ok();
+            }
+            catch (NotFoundException)
+            {
+                return NotFound();
+            }
+        }
+
+    }
+}
---
+++ SP.Service/SP.Service.API/ViewModels/ModelErrorVM.cs
@@ -0,0 +1,16 @@
+﻿namespace SP.Customer.API.ViewModels
+{
+    public class ModelErrorVM
+    {
+        public string Type { get; set; }
+        public string Title { get; set; }
+        public int Status { get; set; }
+        public string TraceId { get; set; }
+        public Errors Errors { get; set; }
+    }
+
+    public class Errors
+    {
+        public string[] ExampleProperty { get; set; }
+    }
+}
